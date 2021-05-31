13c07368c97fea7e98382605f350768f816b2318
xgschwi <xaviergschwind@gmail.com>
2021-05-31 13:44:46 -0400

    Exercise 4.4 Completed

--- part4/tests/test.js
+++ part4/tests/test.js
@@ -6,3 +6,68 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithNoBlog = []
+
+  const listWithManyBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a67gdg4646f8',
+      title: 'Go To Another Wiki',
+      author: 'Coco H',
+      url: 'http://www.wikipedia.com/tests',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676fg344434',
+      title: 'Go To Another Place',
+      author: 'Bella H',
+      url: 'http://www.wikipedia.com/FullStack',
+      likes: 6,
+      __v: 0
+    },
+    {
+      _id: '5a42235351b54a676234d17f8',
+      title: 'Go To Something Else',
+      author: 'Gigi H',
+      url: 'http://www.wikipedia.com',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(listWithNoBlog)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })  
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    expect(result).toBe(16)
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,18 @@
-const dummy = (blogs) => {
-    return 1
-  }
+const dummy = (blogs) => 1
   
+const totalLikes = (blogs) => {
+
+  if (blogs.length === 0) return 0
+  else if(blogs.length === 1) return blogs[0].likes
+  else {
+    const x = {}
+    return blogs.reduce((acc, blog) => {
+      return {likes: acc.likes + blog.likes}
+    }).likes
+}
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
