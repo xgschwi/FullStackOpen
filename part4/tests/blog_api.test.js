a08e09f2ed173cdfa404d0513f9cdf63fb9a5b29
xgschwi <xaviergschwind@gmail.com>
2021-06-01 16:35:45 -0400

    Exercise 4.9 Completed

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -4,32 +4,34 @@ const app = require('../app')
 const Blog = require('../models/blog')
 const api = supertest(app)
 
+
 const initialBlogs = [
-    {
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5
-    },
-    {
-      title: 'Go To Another Wiki',
-      author: 'Coco H',
-      url: 'http://www.wikipedia.com/tests',
-      likes: 3
-    },
-    {
-      title: 'Go To Another Place',
-      author: 'Coco H',
-      url: 'http://www.wikipedia.com/FullStack',
-      likes: 6
-    },
-    {
-      title: 'Go To Something Else',
-      author: 'Gigi H',
-      url: 'http://www.wikipedia.com',
-      likes: 2
-    }
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'Go To Another Wiki',
+    author: 'Coco H',
+    url: 'http://www.wikipedia.com/tests',
+    likes: 3
+  },
+  {
+    title: 'Go To Another Place',
+    author: 'Coco H',
+    url: 'http://www.wikipedia.com/FullStack',
+    likes: 6
+  },
+  {
+    title: 'Go To Something Else',
+    author: 'Gigi H',
+    url: 'http://www.wikipedia.com',
+    likes: 2
+  }
 ]
+
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -40,14 +42,26 @@ beforeEach(async () => {
     })
 })
 
-test('all blogs returned as JSON', async () => {
+describe('Supertesting Backend', () => {
+
+
+  test('all blogs returned as JSON', async () => {
     const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
     expect(response.body).toHaveLength(4)
+  })
+
+  test('unique identifier property of blog is named id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
+
 })
+
+
 
 
 afterAll(() => {
