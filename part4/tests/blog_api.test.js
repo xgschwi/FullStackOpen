86831fe2950514c36392b469d9a23d7a4f32e607
xgschwi <xaviergschwind@gmail.com>
2021-06-01 16:20:21 -0400

    Exercise 4.8 Completed

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5
+    },
+    {
+      title: 'Go To Another Wiki',
+      author: 'Coco H',
+      url: 'http://www.wikipedia.com/tests',
+      likes: 3
+    },
+    {
+      title: 'Go To Another Place',
+      author: 'Coco H',
+      url: 'http://www.wikipedia.com/FullStack',
+      likes: 6
+    },
+    {
+      title: 'Go To Something Else',
+      author: 'Gigi H',
+      url: 'http://www.wikipedia.com',
+      likes: 2
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    initialBlogs.forEach(async (blog) => {
+        const blogObject = new Blog(blog)
+        await blogObject.save()
+    })
+})
+
+test('all blogs returned as JSON', async () => {
+    const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(4)
+})
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})
--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGODB_URL
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URL
+    : process.env.MONGODB_URL
+    
 const PORT = process.env.PORT
 
 module.exports = {MONGODB_URI, PORT}
