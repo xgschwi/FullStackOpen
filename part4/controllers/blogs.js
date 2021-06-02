a90d108ba0638ddb80aa79a47b37b968e04adf83
xgschwi <xaviergschwind@gmail.com>
2021-06-02 01:29:19 -0400

    Exercise 4.17 Completed

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,19 +1,34 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 require('express-async-errors')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     response.json(blogs)
 })
   
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
   
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
+
     if(blog.likes === undefined) blog.likes = 0
     if(blog.title === undefined && blog.url === undefined) response.status(400).end()
     else {
         const result = await blog.save()
+        console.log(result._id)
+        user.blogs = user.blogs.concat(result._id)
+        await user.save()
         response.status(201).json(result)
     }
 })
