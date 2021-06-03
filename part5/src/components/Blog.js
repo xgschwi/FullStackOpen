5351564832626592b2015def4b6d8c55b946e4a3
xgschwi <xaviergschwind@gmail.com>
2021-06-03 02:56:31 -0400

    Exercise 5.7 Completed

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+
+const Blog = ({blog}) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    width: 300
+  }
+
+  const [view, setView] = useState(false)
+
+  return(
+    <div style={blogStyle}>
+      <p>
+         {blog.title} {blog.author}   
+         <button onClick={()=> setView(!view)}>View</button>
+      </p>
+      
+      {view ?
+        <div>
+          <p>{blog.url}</p>
+          <p>Likes {blog.likes}
+            <button>Like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      : null
+      }
+    </div>  
+  )
+}
+
 
 export default Blog
