cbb7974c6e9a400c17fe2d48f9bf5d35f458bd62
xgschwi <xaviergschwind@gmail.com>
2021-06-04 22:49:54 -0400

    Exercise 5.14 Completed

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('Renders blog title and author, not Url or likes by default', () => {
@@ -18,4 +18,27 @@ test('Renders blog title and author, not Url or likes by default', () => {
   expect(component.container).toHaveTextContent('A test blog Xavier G')
   expect(component.container).not.toHaveTextContent('test.someurl.com')
   expect(component.container).not.toHaveTextContent('Likes 3')
+})
+
+test('Renders Url or likes after view button pressed', () => {
+  const blog = {
+    title: 'A test blog',
+    author: 'Xavier G',
+    url: 'test.someurl.com',
+    likes: 3,
+    user: { name: 'Xavier', username: 'root' }
+  }
+
+  const component = render(
+    <Blog blog={blog} user={ { name: 'Xavier', username: 'root' }}/>
+  )
+
+  expect(component.container).not.toHaveTextContent('test.someurl.com')
+  expect(component.container).not.toHaveTextContent('Likes 3')
+
+  const button = component.getByText('View')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent('test.someurl.com')
+  expect(component.container).toHaveTextContent('Likes 3')
 })
